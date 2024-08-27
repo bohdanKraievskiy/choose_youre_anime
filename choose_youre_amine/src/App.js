d8677ae413b14578bbe52c59093d3fadf860ff23
bohdanKraievskiy <bogdan.1233211233213@gmail.com>
2024-08-27 23:41:58 +0300

    first comit

--- choose_youre_amine/src/App.js
+++ choose_youre_amine/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/main.css'
 import AnimeCarousel from './components/main';
 function App() {
+    const handleShareInviteLink = () => {
+        const shareLink = `https://t.me/anime_world_fun_bot`;
+        window.open(shareLink, '_blank');
+    };
   return (
+
       <div className="App">
-        <h1>Anime Game Showcase</h1>
+          <div className="App-header">
+        <div className="_footer_text _coin">Anime World</div>
+              <div className="_footer_text _play" style={{fontSize:"50px"}} onClick={handleShareInviteLink}>PLAY NOW</div>
+          </div>
         <AnimeCarousel/>
+          <footer style={{color:"pink", background: "rgba(0,0,0,.5)",height:60,placeContent:"center",placeItems:"center",marginLeft: "auto",
+              marginRight: "auto",left: 0,
+              right: 0,position:"absolute",bottom:0,}}> Anime World inc. </footer>
       </div>
   );
 }
--- choose_youre_amine/src/components/main.js
+++ choose_youre_amine/src/components/main.js
@@ -1,41 +1,54 @@
 import React from 'react';
 import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import './AnimeCarousel.css';
+import '../styles/main.css';
 
 const AnimeCarousel = () => {
     const images = [
-        `${process.env.PUBLIC_URL}/images/frame1.jpg`,
-        `${process.env.PUBLIC_URL}/images/frame2.jpg`,
-        `${process.env.PUBLIC_URL}/images/frame3.jpg`,
-        // Додайте більше шляхів до зображень
+        `${process.env.PUBLIC_URL}/anime/chainsawman_power.gif`,
+        `${process.env.PUBLIC_URL}/anime/hunterxhunter_killua.gif`,
+        `${process.env.PUBLIC_URL}/anime/blackclover_asta.gif`,
+        `${process.env.PUBLIC_URL}/anime/bleach.gif`,
+        `${process.env.PUBLIC_URL}/anime/image1.jpg`,
+        `${process.env.PUBLIC_URL}/anime/Jujutsu Kaisen.gif`,
+        `${process.env.PUBLIC_URL}/anime/naruto.gif`,
+        `${process.env.PUBLIC_URL}/anime/Naruto_Madara.gif`,
+        `${process.env.PUBLIC_URL}/anime/Ragna.gif`,
+        `${process.env.PUBLIC_URL}/anime/shikimori is not just cute.gif`,
+        `${process.env.PUBLIC_URL}/anime/sololeveling.gif`,
+        `${process.env.PUBLIC_URL}/anime/sololeveling_godofstatue.gif`,
+        `${process.env.PUBLIC_URL}/anime/voleyball.gif`,
+        `${process.env.PUBLIC_URL}/anime/клинок рассекающий демонов.gif`,
+        `${process.env.PUBLIC_URL}/anime/магічка сукуно.gif`,
     ];
 
     const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerMode: true,
-        focusOnSelect: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        pauseOnHover: true
+        dots: true, // Показувати точки навігації
+        infinite: true, // Безкінечне гортання
+        speed: 500, // Швидкість анімації (в мілісекундах)
+        slidesToShow: 6, // Скільки слайдів показувати одночасно
+        slidesToScroll: 3, // Скільки слайдів переміщати при гортанні
+        autoplay: true, // Автоматичне гортання
+        autoplaySpeed: 1000, // Інтервал автоматичного гортання
+        pauseOnHover: true, // Зупинка автоматичного гортання при наведенні
+        centerMode: true, // Центрування активного слайда
+        focusOnSelect: true, // Гортання при натисканні на слайд
+        adaptiveHeight: true // Адаптація висоти каруселі під висоту слайда
     };
-
+    const handleShareInviteLink = () => {
+        const shareLink = `https://t.me/anime_world_fun_bot`;
+        window.open(shareLink, '_blank');
+    };
     return (
         <div className="carousel-container">
             <Slider {...settings}>
                 {images.map((image, index) => (
                     <div key={index} className="frame">
-                        <div className="frame-inner">
-                            <img src={image} alt={`Frame ${index + 1}`} className="frame-img" />
-                        </div>
+                        <img src={image} alt={`Frame ${index + 1}`} className="frame-img"/>
+
                     </div>
                 ))}
             </Slider>
+            <div className="_carousel_text" onClick={handleShareInviteLink}>WHO I`M?</div>
         </div>
     );
 };
