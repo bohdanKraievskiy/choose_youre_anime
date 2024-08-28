2436a6174de0636d8e1f5dbcfd0037d7b7da0be6
bohdanKraievskiy <bogdan.1233211233213@gmail.com>
2024-08-29 00:55:18 +0300

    first comit

--- choose_youre_amine/src/App.js
+++ choose_youre_amine/src/App.js
@@ -17,20 +17,20 @@ function App() {
         window.open(shareLink, '_blank');
     };
     const handleShareInviteLink3 = () => {
-        const shareLink = `https://t.me/anime_world_fun_bot`;
+        const shareLink = `https://x.com/anime_world_aw_`;
         window.open(shareLink, '_blank');
     };
   return (
 
       <div className="App">
           <div className="App-header">
-              <div className="_footer_text _ca" onClick={handleCopyInviteLink}>CA: 2yzfEsYL25coZwSo3hAgYNxHWAJ8Na2H2EyQLocTpump</div>
+              <div className="_footer_text _ca" onClick={handleCopyInviteLink}>CA: &nbsp; &nbsp; 2yzfEsYL25coZwSo3hAgYNxHWAJ8Na2H2EyQLocTpump</div>
               <div className="_footer_text _coin">Anime World</div>
               <div className="_footer_text _play" style={{fontSize:"50px"}} onClick={handleShareInviteLink}>PLAY NOW</div>
           </div>
         <AnimeCarousel/>
           <div className="_sotials">
-              <img style={{width:"5%"}} onClick={handleShareInviteLink2} src={`${process.env.PUBLIC_URL}/Telegram-removebg-preview.png`} className="_sotials_media"/>
+              <img style={{width:"4.2%"}} onClick={handleShareInviteLink2} src={`${process.env.PUBLIC_URL}/Telegram-removebg-preview.png`} className="_sotials_media"/>
               <img onClick={handleShareInviteLink3} src={`${process.env.PUBLIC_URL}/new-twitter-logo-x-with-circle-line-badge_692249-34-removebg-preview.png`} className="_sotials_media"/>
           </div>
           <footer style={{
